9d483072bb368cfe967e806135b745a8cdbc5842
cillian1 <cilliangreene@live.ie>
2019-02-15 15:23:01 +0000

    wow

--- api/models/db.js
+++ api/models/db.js
@@ -2,11 +2,12 @@ var mysql = require('mysql');
 
 //local mysql db connection
 var connection = mysql.createConnection({
-  host: 'mysql://' + process.env.OPENSHIFT_MYSQL_DB_HOST + ':' + process.env.OPENSHIFT_MYSQL_DB_PORT + '/',
-    user: process.env.OPENSHIFT_MYSQL_DB_USERNAME,
-    password: process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
-    database: 'productsDB'
-  
+ host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+ user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+ password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+ port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
+ database : process.env.OPENSHIFT_APP_NAME
+
 });
 connection.connect(function(err) {
     if (err) throw err;
--- index.js
+++ index.js
@@ -6,17 +6,18 @@ const mysql = require('mysql');
 
 app.use(body_parser.urlencoded({extended:true}));
 app.use(body_parser.json());
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_adress = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 const mc = mysql.createConnection({
-  host: 'mysql://' + process.env.OPENSHIFT_MYSQL_DB_HOST + ':' + process.env.OPENSHIFT_MYSQL_DB_PORT + '/',
-    user: process.env.OPENSHIFT_MYSQL_DB_USERNAME,
-    password: process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
-    database: 'productsDB'
+  host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+ user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+ password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+ port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
+ database : process.env.OPENSHIFT_APP_NAME
 });
 
-
+connect to database
 mc.connect();
 
 app.get('/backr',(req,res)=>{
